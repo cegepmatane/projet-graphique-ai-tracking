9bd5c087747c38ee70c0f8c21081725e9d37db86
guillaumedalbignac <dalbignacguillaume@gmail.com>
2021-02-08 14:35:14 -0500

    Ajout d'un compteur de FPS

--- poc/tensorflow webcam/demo.py
+++ poc/tensorflow webcam/demo.py
@@ -2,6 +2,7 @@ from imageai.Detection import ObjectDetection
 import tensorflow as tf
 import os
 import cv2
+import time
 
 #FIX ERREURS CUBLAS
 gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -19,44 +20,45 @@ if gpus:
 execution_path = os.getcwd()
 
 detector = ObjectDetection()
-
 detector.setModelTypeAsRetinaNet()
-#detector.setModelTypeAsYOLOv3()
-#detector.setModelTypeAsTinyYOLOv3()
-
 detector.setModelPath(os.path.join(execution_path, "resnet50_coco_best_v2.1.0.h5"))
-#detector.setModelPath(os.path.join(execution_path, "yolo.h5"))
-#detector.setModelPath(os.path.join(execution_path, "yolo-tiny.h5"))
-
 detector.loadModel(detection_speed="fast")
 
-#custom = detector.CustomObjects(person=True, cell_phone=True, bottle=True, bed=True, book=True, cup=True)   custom_objects=custom,
+custom = detector.CustomObjects(person=False)
+camera = cv2.VideoCapture(0)
+filtreObjets = False
+font = cv2.FONT_HERSHEY_DUPLEX
 
-camera = cv2.VideoCapture(1)
 
+#Main Loop
 while True:
+    start = time.time()
     ret, frame = camera.read()
-    detected_image, detections = detector.detectObjectsFromImage(
-                                                                 input_image=frame, input_type="array",
-                                                                 output_type="array")
-    """
-    for detection in detections:
-        (x1, y1, x2, y2) = detection["box_points"]
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, detection["name"], (x1 + 6, y1 - 6), font, 1.0, (0, 255, 0), 1)"""
+
+    detected_image, detections = detector.detectObjectsFromImage(input_image=frame,
+                                                                 input_type="array",
+                                                                 output_type="array",
+                                                                 minimum_percentage_probability=50)
 
     for eachObject in detections:
-        print(eachObject["name"], " : ", eachObject["percentage_probability"])
-        (x1, y1, x2, y2) = eachObject["box_points"]
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, eachObject["name"], (x1 + 6, y1 - 6), font, 1.0, (0, 255, 0), 1)
-
-    cv2.imshow('Detection', frame)
+            #print(eachObject["name"], " : ", eachObject["percentage_probability"])
+            (x1, y1, x2, y2) = eachObject["box_points"]
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+            cv2.putText(frame, eachObject["name"] + " " + str(round(eachObject["percentage_probability"], 2)) + "%",
+                       (x1 + 6, y1 - 6), font, 1.0, (0, 255, 0), 1)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+    #FPS
+    end = time.time()
+    Inputframes = 30
+    seconds = end - start
+    fps = 1 / (seconds / 5)
+    #print(str(round(seconds, 4)))
+    cv2.putText(frame, str(round(fps)) + " fps", (7, 28), font, 1, (0, 0, 255), 3, cv2.LINE_AA)
+
+    cv2.imshow('Detection', frame)
 
 camera.release()
 cv2.destroyAllWindows()
